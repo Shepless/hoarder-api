60ddeedcc34c4f16b151c8cf15eba631f144082f
Shepless <chris.shepherd1985@gmail.com>
2015-01-25 23:12:17 +0000

    Added winston so we can start logging stuff

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 'use strict';
 
 var server = require.main.require('lib/app'),
-    jobs = require.main.require('lib/jobs');
+    jobs = require.main.require('lib/jobs'),
+    logger = require.main.require('lib/logger');
 
 server.start(function () {
+    logger.info('Hoarder server started');
     jobs.start();
 });
