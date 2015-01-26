183662cee2ddd438dd0e9e5b1e883df391c87977
Shepless <chris.shepherd1985@gmail.com>
2015-01-26 19:51:43 +0000

    Added rekuire to make require internal modules nicer

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 'use strict';
 
-var server = require.main.require('lib/app'),
-    jobs = require.main.require('lib/jobs'),
-    logger = require.main.require('lib/logger');
+var rek = require('rekuire'),
+    foo = rek('hapi'),
+    server = rek('lib/app'),
+    jobs = rek('lib/jobs'),
+    logger = rek('lib/logger');
 
 server.start(function () {
     logger.info('Hoarder server started');
