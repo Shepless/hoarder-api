c98ab63723f08b6782d269b9817147d0a1d3c534
Shepless <chris.shepherd1985@gmail.com>
2015-01-29 23:43:18 +0000

    Started adding proper tvdb jobs to get show and episode updates and check for new episodes

--- index.js
+++ index.js
@@ -3,9 +3,12 @@
 var rek = require('rekuire'),
     server = rek('lib/app'),
     jobs = rek('lib/jobs'),
-    logger = rek('lib/logger');
+    logger = rek('lib/logger'),
+    foo = rek('lib/tvdb/jobs/get-new-episodes');
 
 server.start(function () {
     logger.info('Hoarder server started');
     jobs.start();
+
+    foo();
 });
