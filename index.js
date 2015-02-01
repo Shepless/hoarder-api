707c149109cf1ac1157c1d6d0f6ac2be0d24bbfa
Shepless <chris.shepherd1985@gmail.com>
2015-02-01 20:01:59 +0000

    Started implemention of event manager

--- index.js
+++ index.js
@@ -2,6 +2,9 @@
 
 var rek = require('rekuire'),
     JobManager = rek('lib/job-manager'),
+    EventManager = rek('lib/event-manager'),
+    Notifications = rek('lib/notifications'),
+    TorrentManager = rek('lib/torrent-manager'),
     logger = rek('lib/logger'),
     server = rek('lib/app');
 
@@ -9,8 +12,11 @@ server.start(function () {
     logger.info('Hoarder server started');
 
     JobManager.configure();
+    EventManager.configure();
+    TorrentManager.configure();
+    Notifications.configure();
     JobManager.run(JobManager.JOB_NAMES.CHECK_FOR_UPCOMING_EPISODES);
-    JobManager.run(JobManager.JOB_NAMES.GET_NEW_EPISODES);
-    JobManager.run(JobManager.JOB_NAMES.GET_SHOW_UPDATES);
-    JobManager.run(JobManager.JOB_NAMES.GET_EPISODE_UPDATES);
+    //JobManager.run(JobManager.JOB_NAMES.GET_NEW_EPISODES);
+    //JobManager.run(JobManager.JOB_NAMES.GET_SHOW_UPDATES);
+    //JobManager.run(JobManager.JOB_NAMES.GET_EPISODE_UPDATES);
 });
